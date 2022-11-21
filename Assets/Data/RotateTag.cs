ad03dda6d695d61f56fdfb4e32633a549b8a40ad
SatouTappei <stutpi.signup@gmail.com>
2022-11-21 11:44:39 +0900

    弾発射

---
+++ Assets/Data/RotateTag.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct RotateTag : IComponentData
+{
+    [Header("回転速度")]
+    public float _speed;
+}
---
+++ Assets/System/RotationSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+public class RotationSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((ref Rotation rot, in RotateTag tag) =>
+            {
+                quaternion normalizedRot = math.normalizesafe(rot.Value);
+                quaternion angleToRotate = quaternion.AxisAngle(math.up(), tag._speed * deltaTime);
+
+                rot.Value = math.mul(normalizedRot, angleToRotate);
+            }).Run();
+
+        return inputDeps;
+    }
+}
