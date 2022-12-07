662cbb090cf27b0d7fa1f57cc356566cdd48a3dc
SatouTappei <stutpi.signup@gmail.com>
2022-12-07 14:08:52 +0900

    あ

---
+++ Assets/Example.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class Example : MonoBehaviour
+{
+    void Start()
+    {
+        Do();
+    }
+
+    void Do()
+    {
+        // Allocator.TempJobはジョブが終わったら解放する、結果を受け取る事はない
+        NativeArray<float> resultArray = new NativeArray<float>(1, Allocator.TempJob);
+
+        // 生成 & 初期化
+        SimpleJob job = new SimpleJob
+        {
+            a = 5f,
+            result = resultArray
+        };
+
+        AnorherJob secondJob = new AnorherJob();
+        secondJob.result = resultArray;
+
+        // スケジュール
+        JobHandle handle = job.Schedule();
+        JobHandle secondHandle = secondJob.Schedule(handle);
+
+        secondHandle.Complete();
+
+        float resultingValue = resultArray[0];
+        Debug.Log(resultingValue);
+        Debug.Log(job.a);
+
+        resultArray.Dispose();
+    }
+
+    struct SimpleJob : IJob
+    {
+        public float a;
+        public NativeArray<float> result;
+        
+        public void Execute()
+        {
+            result[0] = a;
+            a = 23;
+        }
+    }
+
+    struct AnorherJob : IJob
+    {
+        public NativeArray<float> result;
+
+        public void Execute()
+        {
+            result[0] = result[0] + 1;
+        }
+    }
+}
--- Assets/System/WaveSystem.cs
+++ Assets/System/WaveSystem.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Jobs;
 
-public class WaveSystem : ComponentSystem
+public class WaveSystem : JobComponentSystem
 {
-    protected override void OnUpdate()
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        Entities.ForEach((ref Translation trans, ref MoveSpeedData data) =>
+        float elapsedTime = (float)Time.ElapsedTime;
+
+        JobHandle jobHandle = Entities.ForEach((ref Translation trans, in MoveSpeedData data, in WaveData waveData) =>
         {
-            float zPos = math.sin((float)Time.ElapsedTime * data.Value);
+            float zPos = waveData.amplitude * math.sin(elapsedTime * data.Value +
+                trans.Value.x * waveData.xOffset + trans.Value.y * waveData.yOffset);
             trans.Value = new float3(trans.Value.x, trans.Value.y, zPos);
-        });
+        }).Schedule(inputDeps);
+
+        return jobHandle;
     }
 }
